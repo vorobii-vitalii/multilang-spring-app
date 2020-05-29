d7eb9cfe3e68d4381f266b981d8160791c5a1ec5
vorobii-vitalii <vitalik.vorobij15@gmail.com>
2020-05-29 21:17:01 +0300

    Added main application class and test for it

---
+++ src/main/java/com/multilang/app/Multilangapp/MultilangAppApplication.java
@@ -0,0 +1,13 @@
+package com.multilang.app.Multilangapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultilangAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultilangAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/multilang/app/Multilangapp/MultilangAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multilang.app.Multilangapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultilangAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
