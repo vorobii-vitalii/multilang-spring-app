ea8634ffb3a1d700a3e7ba0562865ce40478aa26
vorobii-vitalii <vitalik.vorobij15@gmail.com>
2020-05-30 17:50:21 +0300

    Added all-args constructor in Locate for convenient testing

--- src/main/java/com/multilang/app/Multilangapp/entity/Locate.java
+++ src/main/java/com/multilang/app/Multilangapp/entity/Locate.java
@@ -1,5 +1,6 @@
 package com.multilang.app.Multilangapp.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -8,6 +9,7 @@ import javax.persistence.*;
 @Entity
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "locate")
 public class Locate {
 
