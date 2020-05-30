ae5eb570d089155bae9b6ece2c2b068747f2aee8
vorobii-vitalii <vitalik.vorobij15@gmail.com>
2020-05-30 14:14:23 +0300

    Added LocateDTO

---
+++ src/main/java/com/multilang/app/Multilangapp/dto/LocateDTO.java
@@ -0,0 +1,16 @@
+package com.multilang.app.Multilangapp.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class LocateDTO {
+
+    private Long id;
+
+    private String languageCode;
+
+}
