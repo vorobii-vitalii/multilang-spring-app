6de7843f976dd2f32368b8e0f8e2eb02cbcf8ad9
vorobii-vitalii <vitalik.vorobij15@gmail.com>
2020-05-30 17:50:58 +0300

    Added LocateRepository.existsByLanguageCode method in Jpa interface

--- src/main/java/com/multilang/app/Multilangapp/repository/LocateRepository.java
+++ src/main/java/com/multilang/app/Multilangapp/repository/LocateRepository.java
@@ -2,9 +2,12 @@ package com.multilang.app.Multilangapp.repository;
 
 import com.multilang.app.Multilangapp.entity.Locate;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface LocateRepository extends JpaRepository<Locate, Long> {
     Optional<Locate> findByLanguageCode(String languageCode);
+    boolean existsByLanguageCode(String languageCode);
 }
