75ce2492e4d82ba2ea38cc03b6767a0c39ecb513
vorobii-vitalii <vitalik.vorobij15@gmail.com>
2020-05-30 14:23:44 +0300

    Updated LocateServiceImpl to work with LocateService structure

--- src/main/java/com/multilang/app/Multilangapp/service/impl/LocateServiceImpl.java
+++ src/main/java/com/multilang/app/Multilangapp/service/impl/LocateServiceImpl.java
@@ -1,40 +1,49 @@
 package com.multilang.app.Multilangapp.service.impl;
 
+import com.multilang.app.Multilangapp.dto.LocateDTO;
 import com.multilang.app.Multilangapp.entity.Locate;
 import com.multilang.app.Multilangapp.exceptions.LocateNotFoundException;
+import com.multilang.app.Multilangapp.mapper.Mapper;
 import com.multilang.app.Multilangapp.repository.LocateRepository;
 import com.multilang.app.Multilangapp.service.LocateService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class LocateServiceImpl implements LocateService {
 
     private final LocateRepository locateRepository;
+    private final Mapper<Locate, LocateDTO> locateDTOMapper;
 
     @Autowired
-    public LocateServiceImpl(LocateRepository locateRepository) {
+    public LocateServiceImpl(LocateRepository locateRepository, Mapper<Locate, LocateDTO> locateDTOMapper) {
         this.locateRepository = locateRepository;
+        this.locateDTOMapper = locateDTOMapper;
     }
 
     @Override
-    public Locate getById(Long id) {
-        return locateRepository.findById(id)
+    public LocateDTO getById(Long id) {
+        Locate locate = locateRepository.findById(id)
                 .orElseThrow(LocateNotFoundException::new);
+        return locateDTOMapper.to(locate);
     }
 
     @Override
-    public List<Locate> getAll() {
-        return locateRepository.findAll();
+    public List<LocateDTO> getAll() {
+        return locateRepository.findAll().stream()
+                .map(locateDTOMapper::to)
+                .collect(Collectors.toList());
     }
 
     @Override
-    public Locate getByLanguageCode(String languageCode) {
-        return locateRepository
+    public LocateDTO getByLanguageCode(String languageCode) {
+        Locate locate = locateRepository
                 .findByLanguageCode(languageCode)
                 .orElseThrow(LocateNotFoundException::new);
+        return locateDTOMapper.to(locate);
     }
 
 }
