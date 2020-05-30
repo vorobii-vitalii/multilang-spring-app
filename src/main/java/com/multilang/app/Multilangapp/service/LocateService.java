04fc615b7f9c5a9bf03133a4398083ab773b575f
vorobii-vitalii <vitalik.vorobij15@gmail.com>
2020-05-30 14:26:24 +0300

    Expanded LocateService to add new Locates

--- src/main/java/com/multilang/app/Multilangapp/service/LocateService.java
+++ src/main/java/com/multilang/app/Multilangapp/service/LocateService.java
@@ -5,6 +5,7 @@ import com.multilang.app.Multilangapp.dto.LocateDTO;
 import java.util.List;
 
 public interface LocateService {
+    LocateDTO add(LocateDTO locateDTO);
     LocateDTO getById(Long id);
     List<LocateDTO> getAll();
     LocateDTO getByLanguageCode(String languageCode);
