5627b0c04e59ddaf4b07deba97df43cfae084232
vorobii-vitalii <vitalik.vorobij15@gmail.com>
2020-05-31 20:01:39 +0300

    Added TranslationMapperTest

---
+++ src/test/java/com/multilang/app/Multilangapp/TranslationMapperTest.java
@@ -0,0 +1,79 @@
+package com.multilang.app.Multilangapp;
+
+import com.multilang.app.Multilangapp.dto.LocateDTO;
+import com.multilang.app.Multilangapp.dto.TranslatedTextDTO;
+import com.multilang.app.Multilangapp.dto.TranslationDTO;
+import com.multilang.app.Multilangapp.entity.Locate;
+import com.multilang.app.Multilangapp.entity.TranslatedText;
+import com.multilang.app.Multilangapp.entity.Translation;
+import com.multilang.app.Multilangapp.mapper.LocateMapper;
+import com.multilang.app.Multilangapp.mapper.Mapper;
+import com.multilang.app.Multilangapp.mapper.TranslatedTextMapper;
+import com.multilang.app.Multilangapp.mapper.TranslationMapper;
+import com.multilang.app.Multilangapp.repository.LocateRepository;
+import com.multilang.app.Multilangapp.repository.TranslatedTextRepository;
+import com.multilang.app.Multilangapp.service.LocateService;
+import com.multilang.app.Multilangapp.service.TranslatedTextService;
+import com.multilang.app.Multilangapp.service.impl.LocateServiceImpl;
+import com.multilang.app.Multilangapp.service.impl.TranslatedTextServiceImpl;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.doReturn;
+
+@SpringBootTest
+public class TranslationMapperTest {
+
+    @Mock
+    private TranslatedTextRepository translatedTextRepository;
+
+    @Mock
+    private LocateRepository locateRepository;
+
+    private Mapper<Translation, TranslationDTO> translationDTOMapper;
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+        Mapper<Locate, LocateDTO> locateDTOMapper = new LocateMapper();
+        Mapper<TranslatedText, TranslatedTextDTO> translatedTextDTOMapper = new TranslatedTextMapper();
+        LocateService locateService = new LocateServiceImpl(locateRepository, locateDTOMapper);
+        TranslatedTextService translatedTextService = new TranslatedTextServiceImpl(translatedTextRepository, translatedTextDTOMapper);
+        translationDTOMapper = new TranslationMapper(translatedTextService, locateService, locateDTOMapper, translatedTextDTOMapper);
+    }
+
+    @Test
+    public void testTranslationMapperFrom() {
+
+        // Construct DTO object
+
+        final Long id = 2L;
+        final Long translationTextId = 1L;
+        final String text = "Some text";
+        final String languageCode = "ua";
+
+        TranslationDTO translationDTO = new TranslationDTO(id, translationTextId, text, languageCode);
+
+        // Assume that TranslationText exists by given id
+
+        doReturn(Optional.of(new TranslatedText(translationTextId))).when(translatedTextRepository).findById(translationTextId);
+
+        // Assume that Locate exists by given id
+
+        doReturn(Optional.of(new Locate(1L, languageCode))).when(locateRepository).findByLanguageCode(languageCode);
+
+        Translation translation = translationDTOMapper.from(translationDTO);
+
+        assertEquals(translation.getId(), translationDTO.getId());
+        assertEquals(translation.getLocate().getLanguageCode(), translationDTO.getLanguageCode());
+        assertEquals(translation.getText(), translationDTO.getText());
+        assertEquals(translation.getTranslatedText().getId(), translationDTO.getTranslatedTextId());
+    }
+
+}
