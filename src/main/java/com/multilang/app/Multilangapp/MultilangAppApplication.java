b5f675da4f17d523ee077698ac5b7bfa206d7d72
vorobii-vitalii <vitalik.vorobij15@gmail.com>
2020-05-31 20:01:09 +0300

    Added Validator bean in the configuration

--- src/main/java/com/multilang/app/Multilangapp/MultilangAppApplication.java
+++ src/main/java/com/multilang/app/Multilangapp/MultilangAppApplication.java
@@ -2,12 +2,21 @@ package com.multilang.app.Multilangapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+import javax.validation.Validator;
 
 @SpringBootApplication
 public class MultilangAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MultilangAppApplication.class, args);
+	}
+
+	@Bean
+	public LocalValidatorFactoryBean validator() {
+		return new LocalValidatorFactoryBean();
 	}
 
 }
