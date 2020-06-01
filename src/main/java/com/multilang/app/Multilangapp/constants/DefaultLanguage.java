675e7b099e574053de0b9c11b2d75b173574e52b
vorobii-vitalii <vitalik.vorobij15@gmail.com>
2020-06-01 16:32:58 +0300

    Updated DefaultLanguage constants interface

--- src/main/java/com/multilang/app/Multilangapp/constants/DefaultLanguage.java
+++ src/main/java/com/multilang/app/Multilangapp/constants/DefaultLanguage.java
@@ -1,8 +1,5 @@
 package com.multilang.app.Multilangapp.constants;
 
-import org.springframework.beans.factory.annotation.Value;
-
 public interface DefaultLanguage {
-    @Value("${multilang.default-lang}")
-    String VALUE = "";
+    String VALUE = "en";
 }
