f3160602d1596268287daa4f39e9cc2dabb8c3a1
vorobii-vitalii <vitalik.vorobij15@gmail.com>
2020-06-01 18:56:17 +0300

    Added ArticleDTO

---
+++ src/main/java/com/multilang/app/Multilangapp/dto/ArticleDTO.java
@@ -0,0 +1,22 @@
+package com.multilang.app.Multilangapp.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ArticleDTO {
+
+    private Long id;
+
+    private TranslationDTO title;
+
+    private TranslationDTO body;
+
+    private Date createdAt;
+
+}
