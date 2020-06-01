8bf146a69e1adfadee41fa53bce5539c38a9143b
vorobii-vitalii <vitalik.vorobij15@gmail.com>
2020-06-01 16:32:40 +0300

    Added all-args constructor in Translation entity class

--- src/main/java/com/multilang/app/Multilangapp/entity/Translation.java
+++ src/main/java/com/multilang/app/Multilangapp/entity/Translation.java
@@ -1,12 +1,16 @@
 package com.multilang.app.Multilangapp.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
 @Entity
 @Table(name = "translation")
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Translation {
 
     @Id
@@ -14,15 +18,15 @@ public class Translation {
     @Column(name = "id")
     private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "translated_text_id")
-    private TranslatedText translatedText;
-
     @Column(name = "text")
     private String text;
 
     @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "locate_id")
     private Locate locate;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "translated_text_id")
+    private TranslatedText translatedText;
 
 }
