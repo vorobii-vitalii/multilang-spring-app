f310a0459abc60d43427fcc01821e73f37218d4b
vorobii-vitalii <vitalik.vorobij15@gmail.com>
2020-06-01 19:37:04 +0300

    Added ArticleMapper

---
+++ src/main/java/com/multilang/app/Multilangapp/mapper/ArticleMapper.java
@@ -0,0 +1,44 @@
+package com.multilang.app.Multilangapp.mapper;
+
+import com.multilang.app.Multilangapp.dto.ArticleDTO;
+import com.multilang.app.Multilangapp.entity.Article;
+import com.multilang.app.Multilangapp.entity.TranslatedText;
+import com.multilang.app.Multilangapp.repository.TranslatedTextRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ArticleMapper implements Mapper<Article, ArticleDTO> {
+
+    private final TranslatedTextRepository translatedTextRepository;
+
+    @Autowired
+    public ArticleMapper(TranslatedTextRepository translatedTextRepository) {
+        this.translatedTextRepository = translatedTextRepository;
+    }
+
+    @Override
+    public Article from(ArticleDTO articleDTO) {
+        Article article = new Article();
+        article.setId(articleDTO.getId());
+        article.setCreatedAt(articleDTO.getCreatedAt());
+
+        TranslatedText translatedTitle = translatedTextRepository.getOne(articleDTO.getTitle().getId());
+        TranslatedText translatedBody = translatedTextRepository.getOne(articleDTO.getBody().getId());
+
+        article.setTranslatedTitle(translatedTitle);
+        article.setTranslatedBody(translatedBody);
+
+        return article;
+    }
+
+    @Override
+    public ArticleDTO to(Article article) {
+        ArticleDTO articleDTO = new ArticleDTO();
+        articleDTO.setId(article.getId());
+        articleDTO.setCreatedAt(article.getCreatedAt());
+        // Title and body are missed intentionally
+        return articleDTO;
+    }
+
+}
