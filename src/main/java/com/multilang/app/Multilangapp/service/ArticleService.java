001db0e4c55865bb343a3fb3527ac3d726a61f12
vorobii-vitalii <vitalik.vorobij15@gmail.com>
2020-06-01 23:35:10 +0300

    Added ArticleService.getById

--- src/main/java/com/multilang/app/Multilangapp/service/ArticleService.java
+++ src/main/java/com/multilang/app/Multilangapp/service/ArticleService.java
@@ -2,9 +2,8 @@ package com.multilang.app.Multilangapp.service;
 
 import com.multilang.app.Multilangapp.dto.ArticleDTO;
 
-import java.util.List;
-
 public interface ArticleService {
+    ArticleDTO getById(Long id);
     ArticleDTO getByIdAndLanguageCode(Long id, String languageCode);
     ArticleDTO add();
     void deleteById(Long id);
